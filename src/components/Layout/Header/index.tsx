7edb91d641063d11f1d39c4a4d2a45a26b86174a
MuizU <muizuvais99@gmail.com>
2023-03-03 18:18:21 +0530

    Created the header component

---
+++ src/components/Layout/Header/index.tsx
@@ -0,0 +1,11 @@
+import style from './styles.module.sass'
+export default function Header(){
+    return (
+        <header>
+            <div className={style.header}>
+                Header
+                </div>
+        </header>
+    )
+
+}
