34372b48c1361399a73e4cc57736a3f72f74457e
MuizU <muizuvais99@gmail.com>
2023-03-03 19:19:14 +0530

    Added header and footer to layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Footer from "@/components/Layout/Footer";
+import Header from "@/components/Layout/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
