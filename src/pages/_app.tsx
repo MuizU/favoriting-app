3a8013afd720e790dcb2f7674bbbb11dc8f9780a
muizu <muizuvais99@gmail.com>
2023-03-06 15:21:01 +0530

    Changed the font to noto sans

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {Noto_Sans} from "next/font/google";
+
+const noto_Sans = Noto_Sans({weight: ["400", "700"], subsets: ["latin"]});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
+        <style jsx global>{`
+        html {
+          font-family: ${noto_Sans.style.fontFamily};
+        }
+      `}</style>
+        <Header />
       <Component {...pageProps} />
       <Footer />
     </>
