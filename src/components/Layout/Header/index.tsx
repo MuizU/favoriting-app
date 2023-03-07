b718947255ecb3e466f1f9a5ec9223aab0b8f8b5
muizu <muizuvais99@gmail.com>
2023-03-07 20:43:05 +0530

    Created the header component and added styling

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,10 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import style from './styles.module.sass'
-export default function Header(){
+export default function Header() {
     return (
         <header>
             <div className={style.header}>
-                Header
+                <div className={style.headerTitle}>
+                    <h3>
+                        Favoriting App
+                    </h3>
                 </div>
+                <div className={style.headerItems}>
+                    <ul>
+                        <li>
+                            <Link href='/'>
+                                <Image width={24} height={24} src="/icons/home-filled-white.png" alt="" />
+                                <span>Home</span></Link></li>
+                        <li>
+                            <Link href='/favorites'>
+                                <Image width={24} height={24} src="/icons/heart-filled-white.png" alt="" />
+                                <span>Favorites</span></Link></li>
+                    </ul>
+                </div>
+            </div>
         </header>
     )
 
